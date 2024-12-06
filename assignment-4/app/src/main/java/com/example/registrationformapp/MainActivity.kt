da133faa9ccb522f96905b6d511a021f1c70aa97
Irakli <Iraklyoda42@gmail.com>
2024-12-06 10:33:46 +0400

    style: update text sizing method and remove unnecesary placeholders

--- assignment-4/app/src/main/java/com/example/registrationformapp/MainActivity.kt
+++ assignment-4/app/src/main/java/com/example/registrationformapp/MainActivity.kt
@@ -11,8 +11,6 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
