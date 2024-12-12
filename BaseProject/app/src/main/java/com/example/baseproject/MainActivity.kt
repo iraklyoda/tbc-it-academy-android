ed935f4dd8d783a8184e188b7951eb01215862d8
Irakli <Iraklyoda42@gmail.com>
2024-12-12 21:44:25 +0400

    refactor: get string values using context

--- BaseProject/app/src/main/java/com/example/baseproject/MainActivity.kt
+++ BaseProject/app/src/main/java/com/example/baseproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.baseproject
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.baseproject.databinding.ActivityMainBinding
@@ -12,27 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setText()
+        setTexts(context = this, binding = binding)
     }
+}
 
-    private fun setText() {
-        binding.textAndes?.text = getString(R.string.andes_mountain)
-        binding.textSouthAmerica.text = getString(R.string.south_america)
-        binding.textPrice?.text = getString(R.string.price)
-        binding.textDollarSign?.text = getString(R.string.dollar_sign)
-        binding.text230?.text = getString(R.string._230)
+fun setTexts(context: Context, binding: ActivityMainBinding) {
+    binding.textAndes?.text = context.getString(R.string.andes_mountain)
+    binding.textSouthAmerica.text = context.getString(R.string.south_america)
+    binding.textPrice?.text = context.getString(R.string.price)
+    binding.textDollarSign?.text = context.getString(R.string.dollar_sign)
+    binding.text230?.text = context.getString(R.string._230)
 
-        binding.textOverview?.text = getString(R.string.overview)
-        binding.textDetails?.text = getString(R.string.details)
+    binding.textOverview?.text = context.getString(R.string.overview)
+    binding.textDetails?.text = context.getString(R.string.details)
 
-        binding.text8Hours?.text = getString(R.string._8_hours)
-        binding.textTemp?.text = getString(R.string._16_c)
-        binding.textReview?.text = getString(R.string._4_5)
+    binding.text8Hours?.text = context.getString(R.string._8_hours)
+    binding.textTemp?.text = context.getString(R.string._16_c)
+    binding.textReview?.text = context.getString(R.string._4_5)
 
-        binding.textDescription?.text = getString(R.string.description)
-        binding.textBookNow?.text = getString(R.string.book_now)
-
-    }
+    binding.textDescription?.text = context.getString(R.string.description)
+    binding.textBookNow?.text = context.getString(R.string.book_now)
 }
 
 
