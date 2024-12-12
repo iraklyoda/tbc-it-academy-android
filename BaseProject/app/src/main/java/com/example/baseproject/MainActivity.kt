ae8ca9979e84924f607e16b27b4a6fadc8ee9b60
Irakli <Iraklyoda42@gmail.com>
2024-12-12 19:59:33 +0400

    refactor: re-enter parameters in xml and set text trough controller

--- BaseProject/app/src/main/java/com/example/baseproject/MainActivity.kt
+++ BaseProject/app/src/main/java/com/example/baseproject/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.example.baseproject
 
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-import android.util.TypedValue
-import android.view.ViewGroup
-import android.view.ViewGroup.MarginLayoutParams
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
-import androidx.core.content.ContextCompat
-import androidx.core.view.marginBottom
 import com.example.baseproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,116 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        buildCard(binding)
-        buildNav(binding)
-        buildData(binding)
-        buildDescription(binding)
-        buildButton(binding)
+        setText()
     }
 
-    private fun buildCard(binding: ActivityMainBinding) {
-        val card: CardView = binding.cardDestination
-        val cardLayoutParams = binding.cardDestination.layoutParams as MarginLayoutParams
-        cardLayoutParams.height = resources.getDimensionPixelSize(R.dimen.card_height)
-        cardLayoutParams.marginStart = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-        cardLayoutParams.marginEnd = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-        cardLayoutParams.topMargin = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-        card.radius = 25.5f * resources.displayMetrics.density
+    private fun setText() {
+        binding.textAndes?.text = getString(R.string.andes_mountain)
+        binding.textSouthAmerica.text = getString(R.string.south_america)
+        binding.textPrice?.text = getString(R.string.price)
+        binding.textDollarSign?.text = getString(R.string.dollar_sign)
+        binding.text230?.text = getString(R.string._230)
 
-        val btnLeftArrowParams = binding.btnLeftArrow.layoutParams as MarginLayoutParams
-        btnLeftArrowParams.topMargin = resources.getDimensionPixelSize(R.dimen.icon_margin_top)
-        btnLeftArrowParams.marginStart = resources.getDimensionPixelSize(R.dimen.card_horizontal_margin)
+        binding.textOverview?.text = getString(R.string.overview)
+        binding.textDetails?.text = getString(R.string.details)
 
-        val btnBookmarkParams = binding.btnBookmark.layoutParams as MarginLayoutParams
-        btnBookmarkParams.topMargin = resources.getDimensionPixelSize(R.dimen.icon_margin_top)
-        btnBookmarkParams.marginEnd = resources.getDimensionPixelSize(R.dimen.card_horizontal_margin)
+        binding.text8Hours?.text = getString(R.string._8_hours)
+        binding.textTemp?.text = getString(R.string._16_c)
+        binding.textReview?.text = getString(R.string._4_5)
 
-        binding.frameInfo.setPadding(
-            resources.getDimensionPixelSize(R.dimen.info_frame_padding_start),
-            resources.getDimensionPixelSize(R.dimen.info_frame_padding_vertical),
-            resources.getDimensionPixelSize(R.dimen.info_frame_padding_end),
-            resources.getDimensionPixelSize(R.dimen.info_frame_padding_vertical)
-        )
-
-        binding.textAndes?.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_large))
-        binding.textPrice?.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_medium))
-        binding.textSouthAmerica.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_medium))
-        binding.textDollarSign?.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_20))
-        binding.text230?.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_26))
-
-        val frameInfoParams = binding.frameInfo.layoutParams as MarginLayoutParams
-        frameInfoParams.bottomMargin = resources.getDimensionPixelSize(R.dimen.info_frame_margin_bottom)
-        frameInfoParams.leftMargin = resources.getDimensionPixelSize(R.dimen.info_frame_margin_bottom)
-        frameInfoParams.rightMargin = resources.getDimensionPixelSize(R.dimen.info_frame_margin_bottom)
-        frameInfoParams.height = resources.getDimensionPixelSize(R.dimen.info_frame_height)
-
-
-        val backgroundDrawable = GradientDrawable().apply {
-            shape = GradientDrawable.RECTANGLE
-            setColor(ContextCompat.getColor(this@MainActivity, R.color.eerie_black_30))
-            cornerRadius = 16 * resources.displayMetrics.density
-        }
-
-        binding.frameInfo.background = backgroundDrawable
-
-        val textSouth: MarginLayoutParams = binding.textSouthAmerica.layoutParams as MarginLayoutParams
-        textSouth.marginStart = resources.getDimensionPixelSize(R.dimen.location_margin_left)
-    }
-
-    private fun buildNav(binding: ActivityMainBinding) {
-        val navParams: MarginLayoutParams = binding.navLayout.layoutParams as MarginLayoutParams
-        navParams.marginStart = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-        navParams.topMargin = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-
-        val textDetails: MarginLayoutParams = binding.textDetails?.layoutParams as MarginLayoutParams
-        textDetails.marginStart = resources.getDimensionPixelSize(R.dimen.details_margin_left)
-
-        binding.textOverview?.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_22))
-    }
-
-    private fun buildData(binding: ActivityMainBinding) {
-        val navData: MarginLayoutParams = binding.dataLayout?.layoutParams as MarginLayoutParams
-        navData.marginStart = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-        navData.marginEnd = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-        navData.topMargin = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-
-        val textHours: MarginLayoutParams = binding.text8Hours?.layoutParams as MarginLayoutParams
-        textHours.marginStart = resources.getDimensionPixelSize(R.dimen.location_margin_left)
-
-        val textTemp: MarginLayoutParams = binding.textTemp?.layoutParams as MarginLayoutParams
-        textTemp.marginStart = resources.getDimensionPixelSize(R.dimen.location_margin_left)
-
-        val textReview: MarginLayoutParams = binding.textReview?.layoutParams as MarginLayoutParams
-        textReview.marginStart = resources.getDimensionPixelSize(R.dimen.location_margin_left)
-    }
-
-    private fun buildDescription(binding: ActivityMainBinding) {
-        val textDescriptionParams: MarginLayoutParams = binding.textDescription?.layoutParams as MarginLayoutParams
-        textDescriptionParams.topMargin = resources.getDimensionPixelSize(R.dimen.details_margin_left)
-        binding.textDescription.setTextSize(TypedValue.COMPLEX_UNIT_PX, resources.getDimension(R.dimen.text_medium))
-    }
-
-    private fun buildButton(binding: ActivityMainBinding) {
-        val buttonLayoutParams = binding.buttonLayout?.layoutParams as MarginLayoutParams
-        buttonLayoutParams.height = resources.getDimensionPixelSize(R.dimen.button_height)
-        buttonLayoutParams.marginStart = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-        buttonLayoutParams.marginEnd = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-        buttonLayoutParams.topMargin = resources.getDimensionPixelSize(R.dimen.horizontal_margin)
-
-        val backgroundDrawable = GradientDrawable().apply {
-            shape = GradientDrawable.RECTANGLE
-            setColor(ContextCompat.getColor(this@MainActivity, R.color.black))
-            cornerRadius = 16 * resources.displayMetrics.density
-        }
-
-        binding.buttonLayout.background = backgroundDrawable
-        binding.textBookNow?.setTextColor(ContextCompat.getColor(this, R.color.white))
-
-        val textBookNowParams = binding.textBookNow?.layoutParams as MarginLayoutParams
-        textBookNowParams.marginEnd = resources.getDimensionPixelSize(R.dimen.info_frame_padding_start)
+        binding.textDescription?.text = getString(R.string.description)
+        binding.textBookNow?.text = getString(R.string.book_now)
     }
 }
-
 
 
 
