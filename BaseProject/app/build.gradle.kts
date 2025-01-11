c5dfc5be1ee5092c5a0fd7e4172f104c4e8b0a3a
Irakli <Iraklyoda42@gmail.com>
2025-01-11 16:41:00 +0400

    refactor: include navigation graph to base project

--- BaseProject/app/build.gradle.kts
+++ BaseProject/app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- BaseProject/app/src/main/java/com/example/baseproject/MainActivity.kt
+++ BaseProject/app/src/main/java/com/example/baseproject/MainActivity.kt
@@ -6,13 +6,10 @@ import com.example.baseproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main, MainFragment())
-            .commit()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
