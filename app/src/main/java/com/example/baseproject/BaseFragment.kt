997d8cf3055fe3ec07a81a9a9f6351860b4ddb9c
Irakli <Iraklyoda42@gmail.com>
2025-01-13 20:46:24 +0400

    feat: include basefragment

---
+++ app/src/main/java/com/example/baseproject/BaseFragment.kt
@@ -0,0 +1,28 @@
+package com.example.baseproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding>(
+    private val inflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+) : Fragment() {
+    private var _binding: VB? = null
+    private val binding: VB get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = inflater(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/baseproject/FirstFragment.kt
+++ app/src/main/java/com/example/baseproject/FirstFragment.kt
@@ -1,21 +1,7 @@
 package com.example.baseproject
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.baseproject.databinding.FragmentFirstBinding
 
-class FirstFragment : Fragment() {
-    private var _binding: FragmentFirstBinding? = null
-    private val binding: FragmentFirstBinding get() = _binding!!
+class FirstFragment : BaseFragment<FragmentFirstBinding>(FragmentFirstBinding::inflate){
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 }
