c014e70261a8af7afc91449371cec3a73a2d9c77
Irakli <Iraklyoda42@gmail.com>
2025-02-10 01:32:27 +0400

    refactor: update baseFragment and dependency versions

--- app/src/main/java/com/example/baseproject/FirstFragment.kt
+++ app/src/main/java/com/example/baseproject/FirstFragment.kt
@@ -3,5 +3,9 @@ package com.example.baseproject
 import com.example.baseproject.databinding.FragmentFirstBinding
 
 class FirstFragment : BaseFragment<FragmentFirstBinding>(FragmentFirstBinding::inflate){
+    override fun start() {
+    }
 
+    override fun listeners() {
+    }
 }
