9a55ebe6233d14932247b00793e0cf6f6fc0e754
Irakli <Iraklyoda42@gmail.com>
2025-02-24 02:37:46 +0400

    refactor: return glide processor to kapt which caused error

--- app/src/main/java/com/example/baseproject/data/di/AppModule.kt
+++
@@ -1,9 +0,0 @@
-package com.example.baseproject.data.di
-
-import com.example.baseproject.common.ApiHelper
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
-
