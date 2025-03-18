cbfe7652b9108a46c1ceb22cc9d21f399eefa439
Irakli <Iraklyoda42@gmail.com>
2025-03-18 17:14:57 +0400

    style: build layouts

---
+++ app/src/main/java/com/iraklyoda/categoryapp/presentation/screen/CategoryFragment.kt
@@ -0,0 +1,15 @@
+package com.iraklyoda.categoryapp.presentation.screen
+
+import com.iraklyoda.categoryapp.databinding.FragmentCategoryBinding
+import com.iraklyoda.categoryapp.presentation.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CategoryFragment : BaseFragment<FragmentCategoryBinding>(FragmentCategoryBinding::inflate){
+    override fun start() {
+    }
+
+    override fun listeners() {
+        
+    }
+}
